784bb33b93c32f4443751fae39a3db69db927cfb
Stefano Metafuni <metafunistefano@gmail.com>
2021-10-09 09:26:15 +0100

    fallback blocking

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -37,7 +37,7 @@ export async function getStaticPaths() {
                 meetupId: meetup._id.toString()
             }
         })),
-        fallback: false
+        fallback: 'blocking'
     }
 }
 
